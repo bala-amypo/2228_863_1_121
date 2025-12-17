b0c286e880faa2c653621ce929324368dc0a0d71
bala-amypo <bala@amypo.in>
2025-12-17 04:58:56 +0000

    User commit

---
+++ src/main/java/com/example/demo/model/Student.java
@@ -0,0 +1,9 @@
+package com.example.demo.model;
+
+import jakarta.presistence.*;
+
+@Entity 
+public class Student{
+    @Id
+    @GeneratedValue
+}
--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -1,1 +1,20 @@
-package com.example.demo.model
+package com.example.demo.model;
+
+import jakarta.presistence.*;
+
+@Entity  
+@Table(name="users")
+
+public class User{
+    @Id
+    @GeneratedValue
+
+    private long id;
+
+    @column(unique = true)
+
+    private String email;
+    private String name;
+    private String password;
+    private String role ="Staff";
+}
