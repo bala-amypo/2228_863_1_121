ee93931515a6ec65373158e0ad07a2301cce3428
bala-amypo <bala@amypo.in>
2025-12-17 05:18:56 +0000

    User commit

--- src/main/java/com/example/demo/model/SeatingPlan.java
+++ src/main/java/com/example/demo/model/SeatingPlan.java
@@ -1,3 +1,25 @@
 package com.example.demo.model;
 
-import ja
+import jakarta.prepersist;
+import java.time.LocalDateTime ;
+
+@Entity
+public class SeatingPlan
+{
+    @Id
+    @GeneratedValue
+    private Long id;
+    @ManyTOne
+
+    private ExamSession examSession;
+
+    @ManyTOne
+    private ExamRoom room;
+
+    @column(length = 5000)
+    private String arrangementjson;
+
+    private LocalDateTime GeneratedAt;
+
+    @preper
+}
