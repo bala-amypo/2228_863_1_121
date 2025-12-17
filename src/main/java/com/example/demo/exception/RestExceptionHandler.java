02206e8974dbf013ccb5ea8adb292f4b49daa6d3
bala-amypo <bala@amypo.in>
2025-12-17 04:53:56 +0000

    User commit

--- src/main/java/com/example/demo/exception/RestExceptionHandler.java
+++ src/main/java/com/example/demo/exception/RestExceptionHandler.java
@@ -1,10 +1,15 @@
 package com.example.demo.exception;
 
 import org.springframework.http.*;
-import org.springframework.web.bind.annotation.*:
+import org.springframework.web.bind.annotation.*;   
 
 @ControllerAdvice
 
 public class RestExceptionHandler{
-    
+    @ExceptionHandler(ApiException.class)
+    public ResponseEntity<String> handle(ApiException ex)
+    {
+        return ResponseEntity.badRequest().body(ex.getMessage());
+    }
+
 }
