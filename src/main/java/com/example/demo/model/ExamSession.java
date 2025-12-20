b9f67b91ad4fdeedced0d56cd64a9ce7266c59c2
bala-amypo <bala@amypo.in>
2025-12-20 16:54:32 +0000

    User commit

--- src/main/java/com/example/demo/model/ExamSession.java
+++ src/main/java/com/example/demo/model/ExamSession.java
@@ -1,17 +1,16 @@
 package com.example.demo.model;
+
 import jakarta.persistence.*;
 import java.time.LocalDate;
 import java.util.Set;
+
 @Entity
 @Table(name = "exam_sessions")
 public class ExamSession {
+
     @Id
-
     @GeneratedValue
-
     private Long id;
-
-
 
     private String courseCode;
 
@@ -19,34 +18,46 @@ public class ExamSession {
 
     private String examTime;
 
-
-
     @ManyToMany
-
     private Set<Student> students;
 
+    public Long getId() {
+        return id;
+    }
 
+    public String getCourseCode() {
+        return courseCode;
+    }
 
-    public Long getId() { return id; }
+    public LocalDate getExamDate() {
+        return examDate;
+    }
 
-    public String getCourseCode() { return courseCode; }
+    public String getExamTime() {
+        return examTime;
+    }
 
-    public LocalDate getExamDate() { return examDate; }
+    public Set<Student> getStudents() {
+        return students;
+    }
 
-    public String getExamTime() { return examTime; }
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-    public Set<Student> getStudents() { return students; }
+    public void setCourseCode(String courseCode) {
+        this.courseCode = courseCode;
+    }
 
+    public void setExamDate(LocalDate examDate) {
+        this.examDate = examDate;
+    }
 
+    public void setExamTime(String examTime) {
+        this.examTime = examTime;
+    }
 
-    public void setId(Long id) { this.id = id; }
-
-    public void setCourseCode(String courseCode) { this.courseCode = courseCode; }
-
-    public void setExamDate(LocalDate examDate) { this.examDate = examDate; }
-
-    public void setExamTime(String examTime) { this.examTime = examTime; }
-
-    public void setStudents(Set<Student> students) { this.students = students; }
-
+    public void setStudents(Set<Student> students) {
+        this.students = students;
+    }
 }
