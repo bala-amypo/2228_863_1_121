f4f7cb0c48227403fd77df9ae1aaa79045929e7f
bala-amypo <bala@amypo.in>
2025-12-20 17:00:31 +0000

    User commit

---
+++ src/main/java/com/example/demo/security/JwtTokenProvider.java
@@ -0,0 +1,22 @@
+package com.example.demo.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import java.util.Date;
+
+public class JwtTokenProvider {
+
+    private final String SECRET = "secretkey";
+    private final long EXPIRY = 86400000;
+
+    public String generateToken(Long userId, String email, String role) {
+        return Jwts.builder()
+                .claim("userId", userId)
+                .claim("email", email)
+                .claim("role", role)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRY))
+                .signWith(SignatureAlgorithm.HS256, SECRET)
+                .compact();
+    }
+}
