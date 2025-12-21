90cf733cf4c65a7343f0e971adb73e735826e98d
bala-amypo <bala@amypo.in>
2025-12-21 14:07:08 +0000

    User commit

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -15,12 +15,10 @@ public class StudentController {
     public StudentController(StudentService service) {
         this.service = service;
     }
-
     @PostMapping
     public Student add(@RequestBody Student student) {
         return service.add(student);
     }
-
     @GetMapping
     public List<Student> all() {
         return service.all();
