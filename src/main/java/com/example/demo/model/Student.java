f80317c01f8430668e3e5726ecb38404fc8d0ee0
bala-amypo <bala@amypo.in>
2025-12-22 03:01:55 +0000

    User commit

--- src/main/java/com/example/demo/model/Student.java
+++ src/main/java/com/example/demo/model/Student.java
@@ -17,10 +17,18 @@ public class Student {
     private String department;
     private Integer year;
 
-    public Long getId() { return id; }
-    public String getRollNumber() { return rollNumber; }
-    public void setRollNumber(String rollNumber) { this.rollNumber = rollNumber; }
-    public String getName() { return name; }
+    public Long getId() {
+         return id;
+          }
+    public String getRollNumber() {
+         return rollNumber; 
+         }
+    public void setRollNumber(String rollNumber) { 
+    this.rollNumber = rollNumber; 
+    }
+    public String getName() {
+         return name; 
+         }
     public void setName(String name) { this.name = name; }
     public String getDepartment() { return department; }
     public void setDepartment(String department) { this.department = department; }
