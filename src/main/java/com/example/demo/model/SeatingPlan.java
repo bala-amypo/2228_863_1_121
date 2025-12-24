19f59c14d3021cbfd884c5f1f5bea950b924520c
bala-amypo <bala@amypo.in>
2025-12-24 06:11:15 +0000

    User commit

--- src/main/java/com/example/demo/model/SeatingPlan.java
+++ src/main/java/com/example/demo/model/SeatingPlan.java
@@ -1,72 +1,22 @@
-package com.example.demo.model;
-
-import jakarta.persistence.*;
-
-import java.time.LocalDateTime;
-
 @Entity
-@Table(name = "seating_plans")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class SeatingPlan {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue
     private Long id;
 
-    @ManyToOne(optional = false)
+    @ManyToOne
     private ExamSession examSession;
 
-    @ManyToOne(optional = false)
+    @ManyToOne
     private ExamRoom room;
 
     @Column(columnDefinition = "TEXT")
     private String arrangementJson;
 
     private LocalDateTime generatedAt;
-
-    public SeatingPlan() {}
-
-    @PrePersist
-    public void onCreate() {
-        this.generatedAt = LocalDateTime.now();
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public ExamSession getExamSession() {
-        return examSession;
-    }
-
-    public ExamRoom getRoom() {
-        return room;
-    }
-
-    public String getArrangementJson() {
-        return arrangementJson;
-    }
-
-    public LocalDateTime getGeneratedAt() {
-        return generatedAt;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setExamSession(ExamSession examSession) {
-        this.examSession = examSession;
-    }
-
-    public void setRoom(ExamRoom room) {
-        this.room = room;
-    }
-
-    public void setArrangementJson(String arrangementJson) {
-        this.arrangementJson = arrangementJson;
-    }
-
-    public void setGeneratedAt(LocalDateTime generatedAt) {
-        this.generatedAt = generatedAt;
-    }
 }
