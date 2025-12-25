b3404c1671cfe9db622f9e8f1ffea82cfb522403
bala-amypo <bala@amypo.in>
2025-12-25 12:47:50 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/SeatingPlanServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/SeatingPlanServiceImpl.java
@@ -1,12 +1,31 @@
- package com.example.demo.service.impl;
+package com.example.demo.service.impl;
 
+import com.example.demo.entity.SeatingPlan;
 import com.example.demo.repository.ExamRoomRepository;
 import com.example.demo.repository.ExamSessionRepository;
 import com.example.demo.repository.SeatingPlanRepository;
 import com.example.demo.service.SeatingPlanService;
+import org.springframework.stereotype.Service;
 
-public class SeatingPlanServiceImpl implements  SeatingPlanService {
-    public SeatingPlanServiceImpl(ExamSessionRepository sessionRepo, SeatingPlanRepository planRepo, ExamRoomRepository roomRepo){
-        super(sessionRepo, planRepo, roomRepo);
+import java.util.List;
+
+@Service
+public class SeatingPlanServiceImpl implements SeatingPlanService {
+
+    private final ExamSessionRepository examSessionRepository;
+    private final SeatingPlanRepository seatingPlanRepository;
+    private final ExamRoomRepository examRoomRepository;
+
+    public SeatingPlanServiceImpl(ExamSessionRepository examSessionRepository,
+                                  SeatingPlanRepository seatingPlanRepository,
+                                  ExamRoomRepository examRoomRepository) {
+        this.examSessionRepository = examSessionRepository;
+        this.seatingPlanRepository = seatingPlanRepository;
+        this.examRoomRepository = examRoomRepository;
+    }
+
+    @Override
+    public List<SeatingPlan> getPlansBySession(Long sessionId) { 
+        return seatingPlanRepository.findByExamSessionId(sessionId);
     }
 }
