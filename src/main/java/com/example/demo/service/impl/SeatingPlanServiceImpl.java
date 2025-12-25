2c50442da29716c2bce4e05b871bfcb2f1a3e393
bala-amypo <bala@amypo.in>
2025-12-25 12:38:49 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/SeatingPlanServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/SeatingPlanServiceImpl.java
@@ -5,7 +5,7 @@ import com.example.demo.repository.ExamSessionRepository;
 import com.example.demo.repository.SeatingPlanRepository;
 import com.example.demo.service.SeatingPlanService;
 
-public class SeatingPlanServiceImpl extends SeatingPlanService {
+public class SeatingPlanServiceImpl implements  SeatingPlanService {
     public SeatingPlanServiceImpl(ExamSessionRepository sessionRepo, SeatingPlanRepository planRepo, ExamRoomRepository roomRepo){
         super(sessionRepo, planRepo, roomRepo);
     }
